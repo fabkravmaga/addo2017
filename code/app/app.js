29562ac37e84aac90d3cca3951d8ee3256b7e611
Fabian Lim <fabianlim1989@gmail.com>
2017-09-16 12:05:59 +0800

    integration with mongolab db

---
+++ code/app/app.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const bodyParser= require('body-parser');
+
+const app = express();
+
+app.use(bodyParser.urlencoded({extended: true}));
+
+const MongoClient = require('mongodb').MongoClient
+
+var db_credentials = '' // retrieve from machine's ENV
+var db
+
+MongoClient.connect(db_credentials, (err, database) => {
+  // ... start the server
+  if (err) return console.log(err)
+  db = database
+  app.listen(3000, () => {
+    console.log('listening on 3000');
+  })
+})
+
+console.log('May Node be with you');
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html');
+})
+
+app.post('/quotes', (req, res) => {
+  db.collection('quotes').save(req.body, (err, result) => {
+    if (err) return console.log(err)
+
+    console.log('saved to database')
+    res.redirect('/')
+  })
+})
