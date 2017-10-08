d3ad9c849ce8bc57f1621e1d3cd27e1503c03538
Fabian Lim <fabianlim1989@gmail.com>
2017-10-08 23:26:39 +0800

    run stage

--- code/docker_run.sh
+++ code/docker_run.sh
@@ -1,16 +1,19 @@
 #!/bin/bash
 
-# clean up
+# clean up so that it can run again
 CTNR_NAME='3jmaster-addo-demo-app'
+NETWORK='isolated_nw'
+SERVER_IP=$(docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' vault-server)
+VAULT_ADDR="http://${SERVER_IP}:8200"
+
 docker kill ${CTNR_NAME} 2>/dev/null
 docker rm ${CTNR_NAME} 2>/dev/null
 
-SERVER_IP=$(docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' vault-server)
-
-export VAULT_ADDR="http://${SERVER_IP}:8200"
-
-docker run --network=isolated_nw \
+docker run -d \
+  --network=${NETWORK} \
+  --restart unless-stopped \
   -p 3000:3000 \
   --name ${CTNR_NAME} \
-  --env VAULT_ADDR \
+  -e VAULT_ADDR \
+  -e VAULT_
   3jmaster/addo-demo-app
