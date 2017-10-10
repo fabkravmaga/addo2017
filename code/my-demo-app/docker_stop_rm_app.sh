219fad6fd839a56e550ca83f52a5c808919f3dcc
Fabian Lim <fabianlim1989@gmail.com>
2017-10-11 00:26:27 +0800

    add error handling

--- code/my-demo-app/docker_stop_rm_app.sh
+++ code/my-demo-app/docker_stop_rm_app.sh
@@ -1,10 +1,19 @@
 #!/bin/bash
 
-export GIT_ROOT=$(git rev-parse --show-toplevel)
 export CTNR_NAME='3jmaster-addo-demo-app'
 export IMG_NAME='3jmaster/addo-demo-app'
 
 # clean up
 echo -e "Cleaning up previous containers."
-docker kill ${CTNR_NAME} 2>/dev/null
-docker rm ${CTNR_NAME} 2>/dev/null
+
+if docker kill ${CTNR_NAME}; then
+  echo "docker killed ${CTNR_NAME}"
+else
+  exit 0
+fi
+
+if docker rm ${CTNR_NAME}; then
+  echo "docker removed ${CTNR_NAME}"
+else
+  exit 0
+fi
