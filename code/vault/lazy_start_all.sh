a84efaeaa0efc9d94fd9c1c2fb3721dda6cd67aa
Fabian Lim <fabianlim1989@gmail.com>
2017-10-10 11:41:12 +0800

    added lazy start all script for lazy me

---
+++ code/vault/lazy_start_all.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+PWD=$(pwd)
+source ${PWD}/.secret0
+
+# Shutdown all first
+nohup bash 99_shutdown_all.sh
+
+# Start all script in sequence
+nohup bash 1_start_server.sh &
+sleep 5
+nohup bash 2_ready_userpass.sh
+nohup bash 3_ready_approle.sh
+nohup bash 4_test_approle.sh
+nohup bash 5_start_ui.sh
