7cf8f27237d09473227b5b184c33b13a32aa2451
Fabian Lim <fabianlim1989@gmail.com>
2017-10-10 18:52:22 +0800

    variables in docker un script

--- code/my-demo-app/docker_run.sh
+++ code/my-demo-app/docker_run.sh
@@ -10,6 +10,8 @@ SERVER_IP=$(docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}
 
 export VAULT_ADDR="http://${SERVER_IP}:8200"
 
+echo "IN DOCKER RUN: $CI_PROJECT_DIR"
+
 docker run --network=isolated_nw \
   -d  \
   -p 3000:3000 \
