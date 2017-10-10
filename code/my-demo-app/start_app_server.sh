7b291c1b5007836a096ed339d527a772084701a6
Fabian Lim <fabianlim1989@gmail.com>
2017-10-10 23:49:03 +0800

    show env

--- code/my-demo-app/start_app_server.sh
+++ code/my-demo-app/start_app_server.sh
@@ -7,7 +7,8 @@
 command -v ./etc/vault >/dev/null 2>&1 || { echo >&2 "I require ./etc/vault but it's not installed.  Aborting."; exit 1; }
 
 SECRET_PATH='secret/example/mongodb'
-
+echo "VAULT_USERNAME: $VAULT_USERNAME"
+echo "VAULT_ADDR $VAULT_ADDR"
 : "${VAULT_ADDR?Need to set your VAULT_ADDR, usually 'export VAULT_ADDR=http://127.0.0.1:8200'}"
 : "${VAULT_USERNAME?Need to set your VAULT_USERNAME}"
 : "${VAULT_PASSWORD?Need to set your VAULT_PASSWORD}"
