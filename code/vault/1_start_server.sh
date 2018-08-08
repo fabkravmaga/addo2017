78464845aa5280b18f403cfa87264b50a041d68e
Fabian Lim <fabianlim1989@gmail.com>
2018-08-08 20:54:07 +0800

    points to actual config

--- code/vault/1_start_server.sh
+++ code/vault/1_start_server.sh
@@ -4,23 +4,27 @@ NODE_TLS_REJECT_UNAUTHORIZED=1 # Set to 0 to disable TLS (ex. vault deployed wit
 NAME='vault-server'
 NETWORK='isolated_nw'
 
-: "${VAULT_DEV_ROOT_TOKEN_ID?Need to set your VAULT_DEV_ROOT_TOKEN_ID}"
-: "${VAULT_USERNAME?Need to set your VAULT_USERNAME}"
-: "${VAULT_PASSWORD?Need to set your VAULT_PASSWORD}"
+# : "${VAULT_DEV_ROOT_TOKEN_ID?Need to set your VAULT_DEV_ROOT_TOKEN_ID}"
+# : "${VAULT_USERNAME?Need to set your VAULT_USERNAME}"
+# : "${VAULT_PASSWORD?Need to set your VAULT_PASSWORD}"
 
+echo -e "- This runs in detached mode -"
 
-echo -e "- This runs in detached mode so KEEP THIS SHELL RUNNING IN THE BACKGROUND -"
-echo -e "- Press 'Ctrl + C' to KILL -"
-echo -e "- OPEN A NEW SHELL to run the next command -"
-sleep 3
+# At startup, the server will read configuration HCL and JSON files from /vault/config
+# any information passed into VAULT_LOCAL_CONFIG is written into local.json in this directory
+# and read as part of reading the directory for configuration files).
+# Please see Vault's configuration documentation for a full list of options.
+
+# -e 'VAULT_LOCAL_CONFIG={"backend": {"file": {"path": "/vault/file"}}, "default_lease_ttl": "168h", "max_lease_ttl": "720h"}' \
 
 docker run \
+  -d \
   --network=${NETWORK} \
   --cap-add IPC_LOCK \
   -p 8200:8200 \
-  -e "VAULT_DEV_ROOT_TOKEN_ID=${VAULT_DEV_ROOT_TOKEN_ID}" \
-  -e 'VAULT_DEV_LISTEN_ADDRESS=0.0.0.0:8200' \
   -e NODE_TLS_REJECT_UNAUTHORIZED=$NODE_TLS_REJECT_UNAUTHORIZED \
   --restart unless-stopped \
+  --volume vault-file:/vault/file:Z \
+  -v "$(pwd)"/vault-server.hcl:/vault/config/vault-server.hcl \
   --name $NAME \
-  vault
+  vault server
