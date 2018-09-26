3e102e4ea01d850898611a6d4af4bfccff3211b3
Fabian Lim <4744132+3jmaster@users.noreply.github.com>
2018-09-26 12:22:33 +0800

    Update 3_ready_approle.sh

--- code/vault/3_ready_approle.sh
+++ code/vault/3_ready_approle.sh
@@ -3,9 +3,9 @@
 # Make sure to have vault on your system before proceeding
 command -v vault >/dev/null 2>&1 || { echo >&2 "I require vault but it's not installed.  Aborting."; exit 1; }
 
-vault policy-write example_r ./policies/example_r.hcl
-vault policy-write example_rw ./policies/example_rw.hcl
-vault policy-write example_approle_admin ./policies/example_approle_admin.hcl
+vault policy write example_r ./policies/example_r.hcl
+vault policy write example_rw ./policies/example_rw.hcl
+vault policy write example_approle_admin ./policies/example_approle_admin.hcl
 
 vault write auth/approle/role/example \
   secret_id_ttl=10m \
